03649b17f5f57d7909be2c1b111c84333aa9dfdf
Yahya Patel <48586810+patelyahya@users.noreply.github.com>
2025-02-21 07:48:03 +0000

    Copilot First Commit

---
+++ 03-Introduction-to-GitHub-Copilot/Projects/Skills/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+void CalculateNumbers(int var1, int var2)
+{
+    Console.WriteLine(var1 + var2);
+}
